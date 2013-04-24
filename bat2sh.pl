76c67b04fedbb22cdd986a447b426b42a6cd4a1c
pancho1862 <pancho_1862@yahoo.com>
2013-04-24 17:37:16 -0300

    Primeros cambios realizados.

--- bat2sh.pl
+++ bat2sh.pl
@@ -1,31 +1,47 @@
 #!/usr/bin/perl
 
-sub cat_ { local *F; open F, $_[0] or return; my @l = <F>; wantarray() ? @l : join '', @l }
-sub output { my $f = shift; local *F; open F, ">$f" or die "output in file $f failed: $!\n"; print F foreach @_; 1 }
+sub cat_ { 
+	local *F; 
+	open F, $_[0] or return; 
+	my @l = <F>; 
+	wantarray() ? @l : join '', @l 
+}
+sub output { 
+	my $f = shift; 
+	local *F; 
+	open F, ">$f" or die "output in file $f failed: $!\n"; 
+	print F foreach @_; 1 
+}
   
 if (@ARGV != 2) {
-    die "Usage: bat2sh.pl <input .bat file> <output .sh file>\n";
+	die "Usage: bat2sh.pl <input .bat file> <output .sh file>\n";
 }
 my @contents = cat_($ARGV[0]);
 
 foreach my $line (@contents) {
 	# rem -> #
-    $line =~ s/rem /# /i;
-    # move -> mv
-    $line =~ s/^\s*move\b/mv/;
-    # del -> rm
-    $line =~ s/^\s*del\b/rm/;
-    # dos eol to unix eol
-    $line =~ s/\r\n/\n/;
-    # set
-    $line =~ s/^\s*set //i;
+	$line =~ s/rem /# /i;
+	# move -> mv
+	$line =~ s/^\s*move\b/mv/;
+	# del -> rm
+	$line =~ s/^\s*del\b/rm/;
+	# dos eol to unix eol
+	$line =~ s/\r\n/\n/;
+	# set
+	$line =~ s/^\s*set //i;
 	# \ -> /
-    $line =~ s/\\/\//g;
-    # %varName% -> $varName
-    while ($line =~ m/(%\w*%)/ig){
+	$line =~ s/\\/\//g;
+	# %varName% -> $varName
+	while ($line =~ m/(%\w*%)/ig){
 		my $word = "\$" . substr($1, 1, -1);
 		$line =~ s/(%\w*%)/$word/ig;
 	}
+	# if ERRORLEVEL 1 goto ERROR -> if [ "$?" = "1" ]; then sub_error fi
+	if ($line =~ m/^\s*if\s*ERRORLEVEL\s*1\s*goto\s*\w*\s*$/i){
+		
+	}
 }
+my $line = "#!/bin/sh\n\n\n";
+unshift @contents, $line;
 
 output $ARGV[1], @contents;
